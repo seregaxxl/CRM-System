c983d7b78b3fde757d7fd066557d4638cb8632c5
Serhii Khvostikov <54321001+seregaxxl@users.noreply.github.com>
2024-12-06 19:09:45 +0100

    Revert "feature/ED-33 reworked according to code review"

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
-import Antd from 'ant-design-vue';
+import './style.css'
 import App from './App.vue'
-import 'ant-design-vue/dist/reset.css';
-import { router } from './router' 
+import { router } from './router' // Adjust path if needed
 
 const app = createApp(App)
-app.use(router) 
-app.use(Antd).mount('#app')
+app.use(router) // Register the router
+app.mount('#app')
